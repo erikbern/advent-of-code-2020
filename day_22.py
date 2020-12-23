415fa35f355978c034b937acb479bc2dadb884bd
Erik Bernhardsson <mail@erikbern.com>
2020-12-22 22:35:58 -0500

    day 22: remove collections.deque

--- day_22.py
+++ day_22.py
@@ -1,7 +1,6 @@
-import collections
 import sys
 
-cards = [collections.deque(), collections.deque()]
+cards = [[], []]
 
 deck = None
 for line in open(sys.argv[1]):
@@ -14,13 +13,14 @@ for line in open(sys.argv[1]):
     else:
         deck.append(int(line.strip()))
 
-original_cards = [c.copy() for c in cards]
+original_cards = cards = (tuple(cards[0]), tuple(cards[1]))
+
 while len(cards[0]) and len(cards[1]):
-    card_1, card_2 = cards[0].popleft(), cards[1].popleft()
-    assert card_1 != card_2
+    card_1, card_2 = cards[0][0], cards[1][0]
     winner = int(card_1 < card_2)
-    cards[winner].append(max(card_1, card_2))
-    cards[winner].append(min(card_1, card_2))
+    new_cards = (max(card_1, card_2), min(card_1, card_2))
+    cards = (cards[0][1:] + (new_cards if card_1 > card_2 else tuple()),
+             cards[1][1:] + (new_cards if card_1 < card_2 else tuple()))
 
 def score(deck):
     n = len(deck)
@@ -34,27 +34,26 @@ def play(cards, depth, max_score_delta):
     has_seen = set()
     while len(cards[0]) and len(cards[1]):
         rounds += 1
-        k = (tuple(cards[0]), tuple(cards[1]))
-        if k in has_seen:
+        if cards in has_seen:
             return 0, None
-        has_seen.add(k)
+        has_seen.add(cards)
         if depth > 0 and abs(score(cards[0]) - score(cards[1])) > max_score_delta:
             break
 
-        round_cards = cards[0].popleft(), cards[1].popleft()
-        if len(cards[0]) >= round_cards[0] and len(cards[1]) >= round_cards[1]:
-            cards_0 = collections.deque(list(cards[0])[:round_cards[0]])
-            cards_1 = collections.deque(list(cards[1])[:round_cards[1]])
-            winner, _ = play([cards_0, cards_1], depth+1, max_score_delta)
+        round_cards = cards[0][0], cards[1][0]
+        if len(cards[0]) >= round_cards[0]+1 and len(cards[1]) >= round_cards[1]+1:
+            cards_0 = cards[0][1:round_cards[0]+1]
+            cards_1 = cards[1][1:round_cards[1]+1]
+            winner, _ = play((cards_0, cards_1), depth+1, max_score_delta)
         else:
             winner = int(round_cards[1] > round_cards[0])
-        cards[winner].append(round_cards[winner])
-        cards[winner].append(round_cards[1-winner])
+        new_cards = (round_cards[winner], round_cards[1-winner])
+        cards = (cards[0][1:] + (new_cards if winner == 0 else tuple()),
+                 cards[1][1:] + (new_cards if winner == 1 else tuple()))
 
     winner = int(score(cards[1]) > score(cards[0]))
     return winner, score(cards[winner])
 
 
 for max_score_delta in range(0, 100000, 1000):
-    cards = [c.copy() for c in original_cards]
-    print('max score delta', max_score_delta, 'winner:', play(cards, 0, max_score_delta))
+    print('max score delta', max_score_delta, 'winner:', play(original_cards, 0, max_score_delta))
